b557c908d4afce74fd2e8cfba6ba73e95dea3f2a
Kris S <ksherre1@asu.edu>
2014-04-28 23:09:49 -0500

    template class coming together, though still in early stages.

--- Source/lib/Classes/CTemplate_class.php
+++ Source/lib/Classes/CTemplate_class.php
@@ -15,6 +15,10 @@ if(!defined('PMC_INIT')){
  * Class CTemplate
  */
 class CTemplate {
+    private $loader;
+    private $envir;
+    private $envir_vars = Array();
+    private $template = Array();
 
     public function __construct(){
         global $gx_config, $gx_library;
@@ -22,10 +26,46 @@ class CTemplate {
         $gx_library->loadLibraryFile($gx_config->config['libpath'].$gx_config->config['class'] , 'Autoloader.php');
         Twig_Autoloader::register();
         //require_once '/path/to/vendor/autoload.php';
-        $loader = new Twig_Loader_String();
-        $twig = new Twig_Environment($loader);
-        echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
+        $this->loader = new Twig_Loader_String();
+        $this->envir = new Twig_Environment($this->loader);
     }
+
+    public function AddTemplate($template, $name=NULL){
+        if(!isset($name)){
+            $name = 'Tid'.sizeof($this->template);
+        }
+        $this->templates[$name] = $this->envir->loadTemplate($template);
+        return $name;
+    }
+
+
+    public function AddVariables($tid, $var, $name = NULL){
+        if(!is_array($var)){
+            if(!isset($name)){
+                //todo add errors
+                return false;
+            }
+            $var = [$name => $var];
+        }
+        if(is_array($this->envir_vars[$tid])){
+            $this->envir_vars[$tid] = array_merge($this->envir_vars[$tid][0],$var);
+        }
+        else{
+            $this->envir_vars[$tid] = $var;
+        }
+        return true;
+    }
+
+    public function RemoveVar($tid, $name){
+        $this->envir_vars[$tid][$name] = NULL;
+        return true;
+    }
+
+    public function RenderTemplates(){
+
+    }
+
+
 }
 
 
